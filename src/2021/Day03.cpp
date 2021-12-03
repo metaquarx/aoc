5f29245da8c71522d4dfc96fede5ecabfb02f1a7
metaquarx <metaquarx@protonmail.com>
2021-12-03 12:06:19 +0000

    Solve 2021/03 part 2

--- src/2021/Day03.cpp
+++ src/2021/Day03.cpp
@@ -38,18 +38,88 @@ static int Part1(const std::vector<std::string> &input) {
 	return gamma_rate * epsilon_rate;
 }
 
+static int Part2(const std::vector<std::string> &input) {
+	auto get_most_common = [](const std::vector<std::string> &read, unsigned index, char bias) {
+		int ones = 0;
+
+		for (auto line : read) {
+			if (line[index] == '1') {
+				ones++;
+			}
+		}
+
+		if (static_cast<int>(read.size()) - ones == ones) {
+			return bias;
+		} else if (static_cast<int>(read.size()) - ones < ones) {
+			return '1';
+		} else {
+			return '0';
+		}
+	};
+
+	auto get_least_common = [&get_most_common](const std::vector<std::string> &read, unsigned index) {
+		if (get_most_common(read, index, '1') == '0') {
+			return '1';
+		} else {
+			return '0';
+		}
+	};
+
+	std::vector<std::string> keep_most_common(input);
+
+	for (unsigned i = 0; i < input[0].size(); i++) {
+		auto most_common = get_most_common(keep_most_common, i, '1');
+
+		for (auto it = keep_most_common.begin(); it < keep_most_common.end();) {
+			if ((*it)[i] != most_common) {
+				it = keep_most_common.erase(it);
+			} else {
+				it++;
+			}
+		}
+
+		if (keep_most_common.size() > 1) {
+			continue;
+		} else {
+			break;
+		}
+	}
+
+	std::vector<std::string> keep_least_common(input);
+
+	for (unsigned i = 0; i < input[0].size(); i++) {
+		auto least_common = get_least_common(keep_least_common, i);
+
+		for (auto it = keep_least_common.begin(); it < keep_least_common.end();) {
+			if ((*it)[i] != least_common) {
+				it = keep_least_common.erase(it);
+			} else {
+				it++;
+			}
+		}
+
+		if (keep_least_common.size() > 1) {
+			continue;
+		} else {
+			break;
+		}
+	}
+
+	return std::stoi(keep_most_common.front(), nullptr, 2) * std::stoi(keep_least_common.front(), nullptr, 2);
+}
+
 
 int main(int argc, char **argv) {
 	std::vector<std::string> test_input = {
 	  "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"};
 	aoc::test(Part1(test_input) == 198, "Part 1 test input");
-	// aoc::test(Part2(test_input) == 900, "Part 2 test input");
+	aoc::test(Part2(test_input) == 230, "Part 2 test input");
 
 	if (auto input = aoc::get_input("inputs/2021/Day03.txt", argc, argv)) {
 		auto formatted = aoc::by_line(*input);
 
 		std::cout << "Part 1: " << Part1(formatted) << std::endl;
-		// std::cout << "Part 2: " << Part2(formatted) << std::endl;
+		std::cout << "Part 2: " << Part2(formatted) << std::endl;
 	} else {
 		return 1;
 	}
